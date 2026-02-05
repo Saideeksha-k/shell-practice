038f626ab185b880b99029fe024ad358147b342c
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-05 20:06:54 +0530

    actions

---
+++ 7.conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt -- greater than
+# -lt -- lessthan
+# -eq -- equal 
+# -ne -- not equal
+
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number is: $NUMBER is greater than 20"
+fi    
