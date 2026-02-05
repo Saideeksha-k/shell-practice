fa5a762dc612e885d299ebe9fcdceb7fec19c823
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-05 18:44:39 +0530

    actions

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Reshma
+PERSON2=Ramesh
+ echo "$PERSON1  :: Hello $PERSON2, How are you?"
+ echo "$PERSON2 :: Hi $PERSON1, I am fine. How are you doing?"
+ echo "$PERSON1  :: doing good. what are you learning?"
+ echo "$PERSON2  ::  I am learning shell, what about you?"
