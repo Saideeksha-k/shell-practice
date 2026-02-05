ef8dac3135759c84ad2c64ac2caae862646bcedb
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-05 18:12:23 +0530

    actions

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ echo "Ram  :: Hello Sita, How are you?"
+ echo "Sita :: Hi Ram, I am fine. How are you doing?"
+ echo "Ram  :: doing good. what are you learning?"
+ echo "Sita ::  I am learning shell, what about you?"
+ 
