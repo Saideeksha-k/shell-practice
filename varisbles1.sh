d13cf30852c69dfa717cf6d5e22b15468c07cc45
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-05 18:53:54 +0530

    actions

--- varisbles1.sh
+++ varisbles1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Ramesh
-PERSON2=Reshma
+PERSON1=$1
+PERSON2=$2
  echo "$PERSON1  :: Hello $PERSON2, How are you?"
  echo "$PERSON2 :: Hi $PERSON1, I am fine. How are you doing?"
  echo "$PERSON1  :: doing good. what are you learning?"
