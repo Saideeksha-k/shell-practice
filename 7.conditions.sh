4d3332263ff4b6ea1b9326bc8737ea7f812cd787
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-05 20:13:57 +0530

    actions

--- 7.conditions.sh
+++ 7.conditions.sh
@@ -9,6 +9,8 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
     echo "Given number is: $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo "Given number is: $NUMBER is equal to 20"
 else
     echo "Given number is: $NUMBER is less than 20"   
 fi    
