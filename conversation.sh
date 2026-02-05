90fd4beb41ae51097774a334ab79ce257673c3ea
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-05 18:26:38 +0530

    actions

--- conversation.sh
+++ conversation.sh
@@ -3,7 +3,7 @@
 PERSON1=Ramesh  # no space between value and =
 PERSON2=Suresh
  echo "$PERSON1  :: Hello $PERSON2, How are you?"
- echo "$PERSSON2 :: Hi $PERSON1, I am fine. How are you doing?"
+ echo "$PERSON2 :: Hi $PERSON1, I am fine. How are you doing?"
  echo "$PERSON1  :: doing good. what are you learning?"
  echo "$PERSON2  ::  I am learning shell, what about you?"
  
