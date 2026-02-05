0d8abd0e6ccf45b1af16df66ea0d350431e557a1
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-05 20:41:31 +0530

    actions

--- 8.installation.sh
+++ 8.installation.sh
@@ -5,10 +5,17 @@ USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
     echo "please run this script with root user"
     exit 1
-fi 
- echo "Installing Nginx"
+fi
+echo "Installing Nginx"
 
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "installing Nginx ....failure"
+    exit 1
+else
+    echo "installing Nginx ....success"
+fi
 
 
 
