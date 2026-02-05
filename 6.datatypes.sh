8658cbc8ffc6b69cd0b1d1999402c50449ca6cd2
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-05 19:56:38 +0530

    actions

--- 6.datatypes.sh
+++ 6.datatypes.sh
@@ -10,6 +10,9 @@ echo "sum is: $SUM"
 
 ## Array ##
 
-FRUITS=("'Apple" "Kiwi" "Guava")
+FRUITS=("Apple" "Kiwi" "Guava")
 
 echo "fruits are: ${FRUITS[@]}"
+echo "first fruit is: ${FRUITS[0]}"
+echo "second fruit is: ${FRUITS[1]}"
+echo "third fruit is: ${FRUITS[2]}"
