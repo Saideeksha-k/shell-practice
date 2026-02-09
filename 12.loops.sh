43693aedd507825cf60d6eabcc73ede605279d44
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-09 14:38:09 +0530

    actions

---
+++ 12.loops.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USER_ID -ne 0 ]; then
+    echo "please run this script with root user" | tee -a $LOGS_FILE
+    exit 1
+fi
+ 
+ 
+ mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ....Failure" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ....Success" | tee -a $LOGS_FILE
+    fi
+}
+
+
+for package in $@ # sudo sh 12.loops.sh nginx mysql nodejs
+do
+    dnf install $package -y
+    VALIDATE $? "$package installation"
+done
+
+
+
