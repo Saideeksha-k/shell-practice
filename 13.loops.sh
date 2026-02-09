df9a0542f9b39a438fbab6631865d96cdc5691ce
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-09 15:54:13 +0530

    actions

--- 13.loops.sh
+++ 13.loops.sh
@@ -25,7 +25,7 @@ VALIDATE(){
 for package in $@ # sudo sh 12.loops.sh nginx mysql nodejs
 do
     dnf list installed $package &>>$LOGS_FILE
-    if [ $? -ne o ]; then
+    if [ $? -ne 0 ]; then
         echo "$package not insalled, intsalling now"
         dnf install $package -y &>>$LOGS_FILE
         VALIDATE $? "$package installation"
